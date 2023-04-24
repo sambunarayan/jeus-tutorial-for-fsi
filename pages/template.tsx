1a3c7121f0afe889275f7209a1622bfd278e86e4
sambunarayan <soyou30257@gmail.com>
2023-04-24 21:51:26 +0900

    Fixed template page

--- pages/template.tsx
+++ pages/template.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Tempate() {
     return (
         <section className="text-gray-600 body-font">
@@ -17,6 +19,26 @@ export default function Tempate() {
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
+                        </div>
+                    </div>
+                    <div className="flex flex-col mb-10 lg:items-start items-center">
+                        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                                <circle cx="6" cy="6" r="3"></circle>
+                                <circle cx="6" cy="18" r="3"></circle>
+                                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                            </svg>
+                        </div>
+                        <div className="flex-grow">
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-3">DB接続設定</h2>
+                            <p className="leading-relaxed text-base">DBユーザーを作成し、新規テーブルを作成してみます。またSQL DeveloperでOracle dbに接続してみます。</p>
+                            <Link href="https://jeus-tutorial-for-fsi.notion.site/DB-DB-b7aa7d2fca0541efa494f2684ad22b07" legacyBehavior>
+                                <a className="mt-3 text-indigo-500 inline-flex items-center">詳細ページへ
+                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </Link>
                         </div>
                     </div>
                     <div className="flex flex-col mb-10 lg:items-start items-center">
